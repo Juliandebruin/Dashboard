ae3fff53e5de90703ece7302c6a61414979e62a8
Juliandebruin <jybruin@avans.nl>
2023-01-17 20:01:08 +0000

    Workig read and write

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,14 +1,23 @@
 import time
+import serial
 import socketio
+
+ser = serial.Serial("/dev/ttyS0", 9600)
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files = {
     '/': './build/',
 })
 
+i = 50
+
 @sio.event
 def connect(sid, environ):
     print(sid, 'connected')
+    ser.write("Connected".encode())
+    print('rpm:     ' + str(i *  80))
+    print('speed:   ' + str(i * 0.8))
+    print('battery: ' + str(i      ))
     sio.emit('rpm'    , {'rpm':   5000}, to=sid)
     sio.emit('speed'  , {'speed':   45}, to=sid)
     sio.emit('battery', {'battery': 80}, to=sid)
