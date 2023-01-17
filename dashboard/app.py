31037962d533a630d5c4479f667852f50e2bafcc
Juliandebruin <jybruin@avans.nl>
2023-01-17 20:23:56 +0000

    Recieving and parsing data correctly

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,26 +1,35 @@
 import time
+import json
 import serial
 import socketio
 
-ser = serial.Serial("/dev/ttyS0", 9600)
+ser = serial.Serial(
+	port='/dev/ttyS0',
+	baudrate = 9600,
+	parity=serial.PARITY_NONE,
+	stopbits=serial.STOPBITS_ONE,
+	bytesize=serial.EIGHTBITS,
+	timeout=1
+)
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files = {
     '/': './build/',
 })
 
-i = 50
+def parse_data(sid):
+    line = ser.readline()
+    data = line.decode('utf-8')
+    if data[0] == '{' and data[-3] == '}':
+        jsonObj = json.loads(data)
+        sio.emit('rpm'    , {'rpm'    : jsonObj['rpm'    ]}, to=sid)
+        sio.emit('speed'  , {'speed'  : jsonObj['speed'  ]}, to=sid)
+        sio.emit('battery', {'battery': jsonObj['battery']}, to=sid)
 
 @sio.event
 def connect(sid, environ):
     print(sid, 'connected')
-    ser.write("Connected".encode())
-    print('rpm:     ' + str(i *  80))
-    print('speed:   ' + str(i * 0.8))
-    print('battery: ' + str(i      ))
-    sio.emit('rpm'    , {'rpm':   5000}, to=sid)
-    sio.emit('speed'  , {'speed':   45}, to=sid)
-    sio.emit('battery', {'battery': 80}, to=sid)
+    parse_data(sid)
 
 @sio.event
 def disconnect(sid):
